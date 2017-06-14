835b44164ed73ddf60d821a1055e4a0fd38f043c
MoonManEDM <moonmanedm@gmail.com>
2017-06-14 12:06:00 -0400

    varying mushrooms
    mushrooms can show 4 states; full, slightly damaged, med dmg, heavy dmg.

--- src/Main.java
+++ src/Main.java
@@ -8,44 +8,60 @@ public class Main extends PApplet{
 	}
 	
 	short scale = 10; //Overall scale of game, tilesize
-	int tiles[][] = new int[30][40];
-	//TILES
-	// 0 = nothing, 1 - 4 = mushroom [full, hit, hit, hit]
-	int mushrooms = 0;
+	int tiles[][] = new int[30][40];		// 0 = nothing, 1 - 4 = mushroom [full, hit, hit, hit]
+	int numMushrooms = 0;
+	
+	
+	
+	
 	
 	//SETTINGS FUNCTION
 	public void settings(){
-		size(9*scale, 16*scale);	//size must be in settings outside of PDE //Aspect Ratio 9:16
+		size(tiles.length*scale, tiles[0].length*scale);	//size must be in settings outside of PDE //Aspect Ratio 9:16
 	}
 	
 	public void setup(){
 		makeMushrooms();
-		
 	}
 	
 	//populates mushrooms onstage
 	public void makeMushrooms(){
 		for(int i = 0; i < tiles.length; i++){
 			for(int j = 0; j < tiles[i].length; j++){
-				if (Math.random() < .1){
-					tiles[i][j] = 1;
+				if (Math.random() < .03){
+					tiles[i][j] = 1;//(int)((Math.random() * 4)+1);
+					numMushrooms++;
 				}
 			}
 		}
 	}
 	
+	//DRAW FUNCTION
 	public void draw(){
+		background(0);
+		
+		//LOOPS THROUGH MUSHROOMS, DRAWS THEM
 		for(int i = 0; i < tiles.length; i++){
 			for(int j = 0; j < tiles[i].length; j++){
+				stroke(150);
+				fill(255);
 				switch (tiles[i][j]){
-				case 0: 
-				case 1: 
-				case 2:
-				case 3:
-				case 4:
-					rect(i*scale,j*scale,scale,scale);
+					case 0: 
+						fill(0);
+						break;
+					case 1: 
+						rect(i*scale,j*scale,scale,scale);
+						break;
+					case 2:
+						rect(i*scale,j*scale,scale,scale*3/4);
+						break;
+					case 3:
+						rect(i*scale,j*scale,scale,scale/2);
+						break;
+					case 4:
+						rect(i*scale,j*scale,scale,scale/4);
+						break;
 				}
-				
 			}
 		}
 	}
